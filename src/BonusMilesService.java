9bf95785fb457206a07d534d52dc4f63e62ac12a
Коробейников Артем <korartol@gmail.com>
2022-06-24 18:33:51 +0300

    Если сумма меньше 1001 мили не начисляются

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,14 @@
 public class BonusMilesService {
     public int calculate(int price) {
-        int amount = 20;
-        int mile = price / amount;
+        int amount;
+        int mile;
+        if (price > 1000) {
+            amount = 20;
+            mile = price / amount;
+        } else {
+            mile = 0;
+        }
+
         return mile;
     }
 }
