3e407b6e1d889f0d4f085beb640e251dda894434
Коробейников Артем <korartol@gmail.com>
2022-06-25 11:01:56 +0300

    Условие в одну строку

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,13 +1,14 @@
 public class BonusMilesService {
     public int calculate(int price) {
-        int amount;
-        int mile;
-        if (price > 1000) {
-            amount = 20;
-            mile = price / amount;
-        } else {
-            mile = 0;
-        }
+//        int amount;
+        int mile = price > 1000 ? price / 20 : 0;
+
+//        if (price > 1000) {
+//            amount = 20;
+//            mile = price / amount;
+//        } else {
+//            mile = 0;
+//        }
 
         return mile;
     }
